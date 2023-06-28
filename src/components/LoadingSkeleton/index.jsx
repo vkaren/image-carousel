fc52bbe56eb4a284c6f0ea62994d38254bb9eea9
krenvp <karenperarnau@gmail.com>
2023-06-28 19:41:23 -0400

    LoadingSkeleton component

---
+++ src/components/LoadingSkeleton/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+function LoadingSkeleton() {
+  return (
+    <section className="skeleton" aria-disabled={true} aria-label="Loading">
+      <div className="section_select-breed"></div>
+      <div className="section_images"></div>
+      <div className="images_selectors"></div>
+    </section>
+  );
+}
+
+export default LoadingSkeleton;
